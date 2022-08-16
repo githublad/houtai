6378b292257930e1b17265ea33b2bc888bc8db82
lad <775280718@qq.com>
2022-08-17 06:52:35 +0800

    4

--- src/main/java/com/atguigu/建造者/AbstractHouseBuilder.java
+++ src/main/java/com/atguigu/建造者/AbstractHouseBuilder.java
@@ -3,7 +3,7 @@ package com.atguigu.建造者;
 /**
  * @version 1.0
  * @author: lad
- * @createTime: 2022-08-02 11:26
+ * @createTime:  2022-08-02 11:26
  */
 public abstract class AbstractHouseBuilder {
     private House house = new House();
